b7213bd67627c3c6d077664964fda0e92e2b15c3
Shanmugasundaram K <shanmugasundaram.tsm@gmail.com>
2025-07-12 19:48:38 +0000

    change the phrase dont have an account?Register with phrase like New use

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -107,9 +107,9 @@ export function LoginForm() {
           </form>
         </Form>
         <div className="mt-4 text-center text-sm">
-            Don't have an account?{' '}
+            New user?{' '}
             <Link href="/auth/register" className="underline">
-              Register
+              Register here
             </Link>
         </div>
       </CardContent>
