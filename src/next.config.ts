a94743d67747e0ef38c35bce6fc4a25104b38d0d
Shanmugasundaram K <shanmugasundaram.tsm@gmail.com>
2025-07-12 16:19:41 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/next.config.ts
@@ -0,0 +1,37 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    // This is to make `pdf-parse` work on the client side
+    if (!isServer) {
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            "fs": false,
+            "path": false,
+            "os": false,
+        }
+    }
+    // This will force the resolution of `pdf-parse` to its browser-compatible version.
+    config.externals.push('pdf-parse');
+    return config
+  }
+};
+
+export default nextConfig;
