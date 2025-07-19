0cfe64da353ba7d5537bbf9313a01efb91a92f0e
Shanmugasundaram K <shanmugasundaram.tsm@gmail.com>
2025-07-19 22:52:22 +0000

    still same error... why did not status updated in database after payment

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -6,4 +6,4 @@ export const FREE_TOPIC_EXAM_LIMIT = 1;
 export const RAZORPAY_KEY_ID = "rzp_test_H9BcFtFwY7XIZb";
 // These secrets should only be used on the server
 export const RAZORPAY_KEY_SECRET = "1u6q7RNwQYHsgbgc6E5afW89";
-export const RAZORPAY_WEBHOOK_SECRET = process.env.RAZORPAY_WEBHOOK_SECRET!;
+export const RAZORPAY_WEBHOOK_SECRET = "Nanaadheera2324";
