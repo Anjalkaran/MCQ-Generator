c8fd7fdf1069467fb33496245f8021412be90360
Shanmugasundaram K <shanmugasundaram.tsm@gmail.com>
2025-07-20 03:42:21 +0000

    why cant you update pro status in darabase

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,5 @@
 
+
 export const ADMIN_EMAIL = "admin@anjalkaran.com";
 export const FREE_TOPIC_EXAM_LIMIT = 1;
 
@@ -7,3 +8,4 @@ export const RAZORPAY_KEY_ID = "rzp_test_H9BcFtFwY7XIZb";
 // These secrets should only be used on the server
 export const RAZORPAY_KEY_SECRET = "1u6q7RNwQYHsgbgc6E5afW89";
 export const RAZORPAY_WEBHOOK_SECRET = "Nanaadheera2324";
+
