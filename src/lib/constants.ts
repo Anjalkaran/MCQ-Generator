90ee1ea8cf2886e02969dfd140d6529540e2eb18
Shanmugasundaram K <shanmugasundaram.tsm@gmail.com>
2025-07-22 17:38:25 +0000

    rzp_live_XNruThYCl1M0Gu   bgLP1VBRdSQ8ZUnpkcUdhriI

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,12 +1,11 @@
 
-
 export const ADMIN_EMAILS = ["admin@anjalkaran.com", "raamamoorthy@gmail.com"];
 export const FREE_EXAM_LIMIT = 5;
 
 // This key is safe to expose on the client-side
-export const RAZORPAY_KEY_ID = "rzp_test_H9BcFtFwY7XIZb";
+export const RAZORPAY_KEY_ID = "rzp_live_XNruThYCl1M0Gu";
 // This secret is used for creating orders and should only be on the server.
-export const RAZORPAY_KEY_SECRET = "1u6q7RNwQYHsgbgc6E5afW89";
+export const RAZORPAY_KEY_SECRET = "bgLP1VBRdSQ8ZUnpkcUdhriI";
 // The webhook secret is loaded directly via process.env in the webhook API route
 // to avoid issues with serverless function environments.
 export const RAZORPAY_WEBHOOK_SECRET = "Nanaadheera@2324";
