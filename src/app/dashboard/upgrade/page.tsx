641df786de1731bebcff384e9d7696d07d7e2573
Shanmugasundaram K <shanmugasundaram.tsm@gmail.com>
2025-07-27 21:37:23 +0000

    set Rs. 799 fee for pa exam

--- src/app/dashboard/upgrade/page.tsx
+++ src/app/dashboard/upgrade/page.tsx
@@ -77,7 +77,7 @@ export default function UpgradePage() {
 
     let price;
     if (userData.examCategory === 'PA') {
-        price = 1;
+        price = 799;
     } else if (userData.examCategory === 'POSTMAN') {
         price = 599;
     } else {
