6f0d7597f2b3d5f5732b9ada321fc398e11413ea
Shanmugasundaram K <shanmugasundaram.tsm@gmail.com>
2025-08-07 02:44:36 +0000

    change Basic Terminologies related to IT to Modernization Project

--- src/lib/exam-blueprints.ts
+++ src/lib/exam-blueprints.ts
@@ -188,7 +188,7 @@ export const PA_BLUEPRINT = {
         {
           sectionName: "Basic terminologies related to IT",
           questions: 10,
-          topics: ["Basic terminologies related to IT Modernisation project of Department of Posts"],
+          topics: ["Modernization Project"],
         },
         {
           sectionName: "Products and Services",
