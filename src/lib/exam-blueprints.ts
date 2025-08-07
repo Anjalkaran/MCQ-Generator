57c60290d445f2b4d36ee2b5c08490398a2e2c51
Shanmugasundaram K <shanmugasundaram.tsm@gmail.com>
2025-08-07 03:25:22 +0000

    under Basic terminologies related to IT category in part A there is a to

--- src/lib/exam-blueprints.ts
+++ src/lib/exam-blueprints.ts
@@ -188,7 +188,7 @@ export const PA_BLUEPRINT = {
         {
           sectionName: "Basic terminologies related to IT",
           questions: 10,
-          topics: ["Basic terminologies related to IT"],
+          topics: [],
         },
         {
           sectionName: "Products and Services",
