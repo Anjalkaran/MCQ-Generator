f5f6e50a1e09ce3690b581d4c881dfca4b3e51e9
Shanmugasundaram K <shanmugasundaram.tsm@gmail.com>
2026-01-07 14:46:05 +0000

    Error: Failed to parse private key: Error: Invalid PEM formatted message

--- src/app/api/version/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-import packageJson from '../../../../package.json';
-
-export const runtime = 'nodejs';
-export const dynamic = 'force-dynamic';
-
-export async function GET() {
-    return NextResponse.json({ version: packageJson.version });
-}
