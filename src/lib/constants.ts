60710ce59cb01698c6a2f03b6d82d8e0ab022fe2
Anjalkaran Admin <shanm@anjalkaran.com>
2026-03-28 23:42:57 +0530

    chore: remove Razorpay keys and configuration as requested

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -6,7 +6,4 @@ export const ADMIN_EMAILS = [
     "shanmugasundaram.tsm@gmail.com"
 ];
 
-// Razorpay constants - using env variables for security
-export const RAZORPAY_KEY_ID = process.env.RAZORPAY_KEY_ID || "";
-export const RAZORPAY_KEY_SECRET = process.env.RAZORPAY_KEY_SECRET || "";
-export const RAZORPAY_WEBHOOK_SECRET = process.env.RAZORPAY_WEBHOOK_SECRET || "";
+// Razorpay constants removed as requested
